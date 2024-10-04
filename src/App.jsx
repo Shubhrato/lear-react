7c10057dd6697ccd2ef24947a69c63b00a1c0203
Shubhrato <shubhrato18@gmail.com>
2024-10-04 16:30:02 +0530

    Learning ReactJs
    Hello everyone,
    I am learning ReactJs from chai with code

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+  return (
+    <>
+      <h1>Counter Value : {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+export default App
